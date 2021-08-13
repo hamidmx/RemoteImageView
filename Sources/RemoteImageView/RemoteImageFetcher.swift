8f9ee5073e3c56e527bcc9e65ecb27e52a56093e
Hamid Elmouloui <hamidmx@gmail.com>
2021-08-13 23:25:20 +0100

    Major change to init

--- Sources/RemoteImageView/RemoteImageFetcher.swift
+++ Sources/RemoteImageView/RemoteImageFetcher.swift
@@ -31,10 +31,10 @@ import SwiftUI
 public class RemoteImageFetcher: ObservableObject {
   @Published var imageData = Data()
   let url: URL
-
-  public init(url: URL) {
-    self.url = url
-  }
+    
+    public init(from url: URL) {
+      self.url = url
+    }
 
   // 1
   public func fetch() {
